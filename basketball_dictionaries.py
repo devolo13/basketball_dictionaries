6eb18f71911e2f0b806a44ac0db1acc156d2da73
devolo13 <devolo13@gmail.com>
2023-04-04 11:55:47 -0500

    initial commit

---
+++ basketball_dictionaries.py
@@ -0,0 +1,84 @@
+class Player:
+    def __init__(self, player_card):
+        self.name = player_card["name"]
+        self.age = player_card["age"]
+        self.position = player_card["position"]
+        self.team = player_card["team"]
+
+    @classmethod
+    def get_team(cls, team_list):
+        team = []
+        for i in range(len(team_list)):
+            team.append('')
+            team[i] = Player(team_list[i])
+        return team
+
+
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Forward",
+        "team": "Philadelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+
+player_kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
+
+roster = []
+for i in range(len(players)):
+    roster.append(None)
+    roster[i] = Player(players[i])
+
+new_roster = Player.get_team(players)
